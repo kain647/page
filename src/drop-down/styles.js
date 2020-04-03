0a34f0d8faf11c609b3ea2eecad2fe6ed5b96b45
Aleksey <aleksey.amelchenko@gmail.com>
2020-04-03 19:57:20 +0300

    Add styles

--- src/drop-down/styles.js
+++ src/drop-down/styles.js
@@ -2,28 +2,27 @@ import styled from "styled-components";
 
 export const DropDownContainer = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
   cursor: pointer;
   flex-direction: column;
   user-select: none;
-  width: 150px;
-  color: gray;
+  width: 100px;
+  color: #fff;
   position: relative;
 `;
 export const CurrentValue = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
+  align-items: center;
+  width: 100px;
   background-color: #ccc;
-  width: 150px;
-  padding: 10px 0 10px 0;
+  padding: 10px 15px 10px 15px;
 `;
 export const ListContainer = styled.div`
   display: flex;
-  width: 100%;
+  width: 130px;
   top: 100%;
   flex-direction: column;
-  background-color: #fff;
+  background-color: #bebebe;
   position: absolute;
 `;
 export const List = styled.div`
